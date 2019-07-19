6367fc83932d0990a9d7300832ff01b30adf7176
Martynas <narijauskasmartynas@gmail.com>
2019-07-20 01:35:40 +0300

    base react proj

--- ppm/src/main/java/io/udemy/ppmtool/services/ProjectService.java
+++ ppm/src/main/java/io/udemy/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.udemy.ppmtool.services;
 
 import io.udemy.ppmtool.domain.Project;
+import io.udemy.ppmtool.exceptions.ProjectIdException;
 import io.udemy.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,36 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdate(Project project){
+    public Project saveOrUpdate(Project project) {
 
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectR epository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
         //All the logic goes there
+    }
 
-        return projectRepository.save(project);
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID " + projectId + " does not exist");
+        } else {
+            return project;
+        }
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete roject widh ID " + projectId + " .It doesnt exist" );
+        }
+        projectRepository.delete(project);
     }
 }
--- ppm/src/main/java/io/udemy/ppmtool/web/ProjectController.java
+++ ppm/src/main/java/io/udemy/ppmtool/web/ProjectController.java
@@ -1,17 +1,19 @@
 package io.udemy.ppmtool.web;
 
 import io.udemy.ppmtool.domain.Project;
+import io.udemy.ppmtool.services.ErrorValidationService;
 import io.udemy.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,14 +22,34 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ErrorValidationService errorValidationService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project,
                                                     BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid project object", HttpStatus.BAD_REQUEST);
-        }
+
+        ResponseEntity<?> errorMap = errorValidationService.CreateMapValidation(result);
+        if(errorMap != null)
+            return errorMap;
 
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        return new ResponseEntity<Project>(projectService.findProjectByIdentifier(projectId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllProjects(){
+        return new ResponseEntity<>(projectService.findAllProjects(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProjectById(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId);
+        return new ResponseEntity<String>("Project with ID: " + projectId + " was deleted", HttpStatus.OK);
     }
 }
